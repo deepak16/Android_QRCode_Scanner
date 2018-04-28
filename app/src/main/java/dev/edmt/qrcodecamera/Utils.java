22928f5ef582cea4a84b01c453db25192d101ce3
deepak16 <deepakkumar.thestar@gmail.com>
2018-04-28 20:51:44 +0530

    first commit

---
+++ app/src/main/java/dev/edmt/qrcodecamera/Utils.java
@@ -0,0 +1,26 @@
+package dev.edmt.qrcodecamera;
+
+import android.os.Handler;
+
+/**
+ * Created by deepak on 30/3/17.
+ */
+public class Utils {
+
+
+    // this is a supporting class for the delay creation
+
+    public interface DelayCallback{
+        void afterDelay();
+    }
+
+    public static void delay(int secs, final DelayCallback delayCallback){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                delayCallback.afterDelay();
+            }
+        }, secs * 1000); // afterDelay will be executed after (secs*1000) milliseconds.
+    }
+}
